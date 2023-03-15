a9d825464380130f811428851e658c1d75c4b04f
iQ <estrizqy@gmail.com>
2023-03-16 01:50:07 +0700

    update operasi pangkat

--- js/script.js
+++ js/script.js
@@ -50,5 +50,19 @@ function kali(){
     }
 }
 
+function pangkat(){
+    var form = document.getElementById('calculator');
+    var a1 = parseFloat(form.angka1.value);
+    var a2 = parseFloat(form.angka2.value);
+    
+    if(isNaN(a1) && isNaN(a2)) {
+        alert("Kolom Tidak Boleh Kosong!");
+    }else{
+        var total = a1 ** a2;
+        form.hasil.value = total;
+    }
+}
+
+
 
 
